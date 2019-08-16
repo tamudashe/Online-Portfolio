7224131028897675a4bfdfff1de5355c111e499c
tamudashe <tamudashe@gmail.com>
2019-08-16 15:41:09 -0400

    done

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let firstHeading = document.getElementById("first-heading");
-console.log(firstHeading.getBoundingClientRect());
-function isVisible(element) {
-    let elementBox = element.getBoundingClientRect();
-    let distanceFromTop = -200;
-    if(elementBox.top - window.innerHeight < distanceFromTop) {return true;}
-    else {return false;}
-}
-function scanDocument() {
-    let sectionList = document.querySelectorAll('.hidden');
-    sectionList.forEach(function(section) {if(isVisible(section)) {section.classList.remove('hidden');};});
-}
-document.addEventListener("scroll", _.throttle(scanDocument, 300));
+// let firstHeading = document.getElementById("first-heading");
+// console.log(firstHeading.getBoundingClientRect());
+// function isVisible(element) {
+//     let elementBox = element.getBoundingClientRect();
+//     let distanceFromTop = -200;
+//     if(elementBox.top - window.innerHeight < distanceFromTop) {return true;}
+//     else {return false;}
+// }
+// function scanDocument() {
+//     let sectionList = document.querySelectorAll('.hidden');
+//     sectionList.forEach(function(section) {if(isVisible(section)) {section.classList.remove('hidden');};});
+// }
+// document.addEventListener("scroll", _.throttle(scanDocument, 300));
