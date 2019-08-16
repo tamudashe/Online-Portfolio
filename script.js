ec462d679c056405d03a8f6e88335c8439565fe1
tamudashe <tamudashe@gmail.com>
2019-08-16 15:18:36 -0400

    scroll fade in

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstHeading = document.getElementById("first-heading");
+
+console.log(firstHeading.getBoundingClientRect());
+
+function isVisible(element) {
+    let elementBox = element.getBoundingClientRect();
+    let distanceFromTop = -200;
+
+    if(elementBox.top - window.innerHeight < distanceFromTop) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function scanDocument() {
+    let sectionList = document.querySelectorAll('.hidden');
+    sectionList.forEach(function(section) {
+        if(isVisible(section)) {
+            section.classList.remove('hidden');
+        };
+    });
+}
+
+document.addEventListener("scroll", _.throttle(scanDocument, 300));
